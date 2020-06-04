de061bd1f05a6f30f76f329512fe1353fd41ddba
Bruno Alonso Palma Ávila <bpalma@LATINSHARE.COM>
2020-06-03 23:22:31 -0400

    controller entity

---
+++ Data/NoLosOlvidesApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NoLosOlvidesApi.Model;
+
+namespace NoLosOlvidesApi.Data
+{
+    public class NoLosOlvidesApiContext : DbContext
+    {
+        public NoLosOlvidesApiContext (DbContextOptions<NoLosOlvidesApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NoLosOlvidesApi.Model.Categoria> Categoria { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using NoLosOlvidesApi.Data;
 
 namespace NoLosOlvidesApi
 {
@@ -26,6 +28,9 @@ namespace NoLosOlvidesApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<NoLosOlvidesApiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("NoLosOlvidesApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
